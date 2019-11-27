b2812c557a4bbc181071afeff32cfc6b43ed7aa4
Vorák Gergő <41107695+xvs921@users.noreply.github.com>
2019-11-27 14:10:17 +0100

    berelheto interface kesz

---
+++ Berelheto.java
@@ -0,0 +1,8 @@
+
+package ingatlanos;
+
+public interface Berelheto {
+    public int mennyibeKerul(int honapokSzama);
+    public boolean  lefoglaltE();
+    public boolean   lefoglal(int foglalniKivantHonapok);
+}
