c6fdac9de4be56c976748a88b0a0c5d2a26c75c3
Vorák Gergő <41107695+xvs921@users.noreply.github.com>
2019-11-27 14:12:28 +0100

    berelheto kesz

---
+++ src/com/java/Berelheto.java
@@ -0,0 +1,8 @@
+
+package com.java;
+
+public interface Berelheto {
+    public int mennyibeKerul(int honapokSzama);
+    public boolean  lefoglaltE();
+    public boolean   lefoglal(int foglalniKivantHonapok);
+}
