03335367d078faa9bd567f8512d42363fdc0ec4b
Levente Nyírő <nyiro.levente@gmail.com>
2019-11-27 14:41:29 +0100

    Update Berelheto.java

--- src/com/java/Berelheto.java
+++ src/com/java/Berelheto.java
@@ -2,7 +2,7 @@ package com.java;
 
 public interface Berelheto 
 {
-    public int mennyibeKerul(int honapokSzama);
+    public double mennyibeKerul(int honapokSzama);
     public boolean lefoglaltE();
     public boolean lefoglal(int foglalniKivantHonapok);
 }
