554045ac7301f1f209446de2658ded30a420b19d
Vorák Gergő <41107695+xvs921@users.noreply.github.com>
2019-11-27 14:59:12 +0100

    CsaladiApartman kijavítva

--- src/com/java/CsaladiApartman.java
+++ src/com/java/CsaladiApartman.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.java;
 
-/**
- *
- * @author Diak
- */
-public class CsaladiApartman {
+public class CsaladiApartman extends Lakas{
+    private int gyerekekSzama;
+
+    public CsaladiApartman(int gyerekekSzama, double terulet, int szobakSzama, int lakok, int nmAr) {
+        super(terulet, szobakSzama, 0, nmAr);
+        this.gyerekekSzama = 0;
+    }
+    
+    public boolean gyerekSzuletik()
+    {
+        boolean megszuletettE=false;
+        if (lakok-gyerekekSzama>=2) {
+            lakok++;
+            gyerekekSzama++;
+            megszuletettE=true;
+        }
+        return megszuletettE;
+    }
+
+    @Override
+    public boolean bekoltozik(int ember) {
+        int felnottekSzama=lakok-gyerekekSzama;
+        double felnottTerulet=10;
+        double gyerekTerulet=5;
+        if (felnottekSzama+gyerekekSzama/2>2) {
+            return false;
+        }
+        else if ((felnottekSzama*felnottTerulet)+(gyerekekSzama*gyerekTerulet)<terulet) {
+            return false;
+        }
+        else
+        {
+            lakok++;
+            return true;           
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+", gyerekek száma=" + gyerekekSzama;
+    }
+
+
+    
+    
+    
+   
     
 }
